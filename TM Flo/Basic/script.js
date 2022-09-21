afc272996d4f38ff1b3ec821a71c27e6d3580e0c
Florian Kürsten <f.kuersten@hotmail.com>
2022-09-21 10:56:25 +0200

    text

--- TM Flo/Basic/script.js
+++ TM Flo/Basic/script.js
@@ -3,4 +3,4 @@ var players = ["Martin", "Thomas", "Peter" , "Matthias" , "Niki"];
 
 console.log(`Hi my name is Martin and I am ${age} years old`);
 
-console.log(`The most valuable player of the match is ` + players[2]);
+console.log(`The most valuable player of the match is ${players[2]}`);
