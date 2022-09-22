19a6d0c0cf04b1860cf449fb0f479a26dbbbc82b
Florian Kürsten <f.kuersten@hotmail.com>
2022-09-22 09:28:00 +0200

    add alex recreate folder

--- TM Flo/Basic/script.js
+++
@@ -1,6 +0,0 @@
-var age = 25;
-var players = ["Martin", "Thomas", "Peter" , "Matthias" , "Niki"];
-
-console.log(`Hi my name is Martin and I am ${age} years old`);
-
-console.log(`The most valuable player of the match is ${players[2]}`);
